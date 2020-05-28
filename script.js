71b483c9f74661ee012ebde121b97c562db0f71f
hedenica <hedenica@hotmail.com>
2020-05-28 18:25:11 -0300

    💾  populating UI with localStorage data

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 
 // Save selected movie index and price
@@ -33,6 +35,26 @@ function updateSelectedCount() {
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Get data from localstorage and populate the UI
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if(selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 //Movie Select Event
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
@@ -50,3 +72,7 @@ container.addEventListener('click', (e) => {
     updateSelectedCount()
   }
 })
+
+// Initial count and total set 
+
+updateSelectedCount();
